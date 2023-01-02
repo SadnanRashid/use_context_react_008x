cad735ad05e50dbe204570d2d8c276aa1b8fbe09
SadnanRashid <sadnan.rashid.07@gmail.com>
2023-01-02 15:01:25 +0600

    s

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { createContext, useContext, useState } from "react";
 
+const TestContext = createContext(null);
 function App() {
+  const [text, setText] = useState("someText");
+
   return (
-    <div className="App">
-      <p>useContext()</p>
-    </div>
+    <TestContext.Provider value={setText}>
+      <div className="App">
+        <p>useContext()</p>
+      </div>
+    </TestContext.Provider>
   );
+}
+
+function kid() {
+  <p></p>;
 }
 
 export default App;
